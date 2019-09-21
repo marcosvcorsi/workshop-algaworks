11d4f98d4a81d87e3747e18c7f77c120865b2053
Marcos Corsi <marcosvcorsi@gmail.com>
2019-09-20 21:46:46 -0300

    CORS added

--- comercial-api/src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ comercial-api/src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.algaworks.comercial.dto.OportunidadeDTO;
 import com.algaworks.comercial.model.Oportunidade;
 import com.algaworks.comercial.repository.OportunidadeRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
